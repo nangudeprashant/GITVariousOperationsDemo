eca5b872be280396a2197a57e68baaed11346591
Administrator <Administrator@DELL>
2020-11-11 16:02:52 +0530

    Initial commit.

---
+++ GITVariousOperationsDemo/src/main/java/com/javalive/GITVariousOperationsDemo/Add.java
@@ -0,0 +1,13 @@
+package com.javalive.GITVariousOperationsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Add 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is addition operation." );
+    }
+}
---
+++ GITVariousOperationsDemo/src/test/java/com/javalive/GITVariousOperationsDemo/AddTest.java
@@ -0,0 +1,38 @@
+package com.javalive.GITVariousOperationsDemo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AddTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AddTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AddTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
