f9b77afd7d16d08942b272729d115dbfdbe48e84
Administrator <Administrator@DELL>
2020-11-11 16:06:23 +0530

    Second commit with Sub class.

---
+++ GITVariousOperationsDemo/src/main/java/com/javalive/GITVariousOperationsDemo/Sub.java
@@ -0,0 +1,8 @@
+package com.javalive.GITVariousOperationsDemo;
+
+public class Sub {
+	public static void main( String[] args )
+    {
+        System.out.println( "This is substraction operation." );
+    }
+}
