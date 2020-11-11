75d9248f8dc04055282a91c1c8780c3c3c969126
Administrator <Administrator@DELL>
2020-11-11 16:09:44 +0530

    Revert "Second commit with Sub class."
    This reverts commit f9b77afd7d16d08942b272729d115dbfdbe48e84.

--- GITVariousOperationsDemo/src/main/java/com/javalive/GITVariousOperationsDemo/Sub.java
+++
@@ -1,8 +0,0 @@
-package com.javalive.GITVariousOperationsDemo;
-
-public class Sub {
-	public static void main( String[] args )
-    {
-        System.out.println( "This is substraction operation." );
-    }
-}
