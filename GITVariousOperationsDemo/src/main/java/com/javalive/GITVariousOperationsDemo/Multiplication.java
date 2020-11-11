9b6c702bc930b6e3dddb5a4bdfcb3db319a07f2e
Administrator <Administrator@DELL>
2020-11-11 16:07:45 +0530

    Multiplication class added.

---
+++ GITVariousOperationsDemo/src/main/java/com/javalive/GITVariousOperationsDemo/Multiplication.java
@@ -0,0 +1,8 @@
+package com.javalive.GITVariousOperationsDemo;
+
+public class Multiplication {
+	public static void main( String[] args )
+    {
+        System.out.println( "This is multiplication operation." );
+    }
+}
